460766a461c45cdd8ac87206d7ddc51eb6db9d28
frosty <aaronfrost@gmail.com>
2019-09-25 02:27:07 -0600

    Commit Step 3 README.MD

---
+++ main.js
@@ -0,0 +1,6 @@
+console.clear();
+
+// rxjs is a global, when loaded how it is load in index.html
+const output = document.querySelector("#output");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
--- main.ts
+++
@@ -1,3 +0,0 @@
-console.clear();
-
-// See the README.MD for what code we will be writing in this section.
