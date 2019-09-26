7cba7e4599b0a3c1e795135660cf2e0a1a0e8f13
frosty <aaronfrost@gmail.com>
2019-09-26 12:33:13 -0600

    Final version

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
 import './styles.scss';
-import { fromEvent } from 'rxjs';
-import { fromFetch } from 'rxjs/internal/observable/dom/fetch';
-import { map, switchMap } from 'rxjs/operators';
+import {
+    AsyncSubject,
+    BehaviorSubject,
+    combineLatest,
+    fromEvent,
+    merge,
+    Observable,
+    of,
+    ReplaySubject,
+    Subject,
+} from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import {
+    catchError,
+    debounceTime,
+    first,
+    map,
+    share,
+    shareReplay,
+    switchMap,
+    take,
+    tap,
+} from 'rxjs/operators';
 import { GIPHY_API_KEY } from './constants';
 import { elements } from './elements';
 
-const searchTermChange$ = fromEvent(elements.search, 'keyup');
+const searchTermChange$ = fromEvent(elements.search, 'keyup').pipe(
+    map(event => (event.target as HTMLInputElement).value),
+);
 const limitLowClick$ = fromEvent(elements.limits.low, 'click');
 const limitMidClick$ = fromEvent(elements.limits.mid, 'click');
-const limitNighClick$ = fromEvent(elements.limits.high, 'click');
+const limitHighClick$ = fromEvent(elements.limits.high, 'click');
 const prevPageClick$ = fromEvent(elements.prevPage, 'click');
 const nextPageClick$ = fromEvent(elements.nextPage, 'click');
 
@@ -16,12 +38,36 @@ const DEFAULT_SEARCH = 'HI';
 const DEFAULT_LIMIT = 10;
 const DEFAULT_PAGE = 0;
 
-const gifsData$ = fromFetch(
-    // prettier-ignore
-    `https://api.giphy.com/v1/gifs/search?q=${DEFAULT_SEARCH}&offset=${DEFAULT_PAGE * DEFAULT_LIMIT}&limit=${DEFAULT_LIMIT}&api_key=${GIPHY_API_KEY}`,
-).pipe(
+const page = new BehaviorSubject(DEFAULT_PAGE);
+const p = page.getValue(); // 100
+
+page.subscribe(val => {
+    //do something
+});
+
+const search$ = new BehaviorSubject(DEFAULT_SEARCH);
+const limit$ = new BehaviorSubject(DEFAULT_LIMIT);
+const page$ = new BehaviorSubject(DEFAULT_PAGE);
+const userPage$ = page$.pipe(map(val => val + 1));
+const totalResults$ = new BehaviorSubject(0);
+const totalPages$: Observable<number> = combineLatest(
+    totalResults$,
+    limit$,
+).pipe(map(([totalResults, limit]) => Math.ceil(totalResults / limit)));
+
+const changes$ = combineLatest(search$, limit$, page$);
+
+const gifsData$ = changes$.pipe(
+    debounceTime(200),
+    switchMap(([search, limit, page]) => {
+        return fromFetch(
+            // prettier-ignore
+            `https://api.giphy.com/v1/gifs/search?q=${search}&offset=${limit * page}&limit=${limit}&api_key=${GIPHY_API_KEY}`,
+        );
+    }),
     // fetch returns a response, and we have to switch to the .json call
     switchMap(response => response.json()),
+    tap(res => totalResults$.next(res.pagination.total_count)),
 );
 
 // Remove just the gifs data from the response
@@ -37,4 +83,54 @@ gifs$.subscribe(gifs => {
         img.src = gif.images.fixed_height_small.url;
         elements.gifContainer.appendChild(img);
     });
+});
+
+// Wire up DOM to BehaviorSubjects
+searchTermChange$.pipe(tap(value => search$.next(value))).subscribe();
+
+search$
+    .pipe(tap(val => ((elements.search as HTMLInputElement).value = val)))
+    .subscribe();
+totalResults$
+    .pipe(tap(val => (elements.totalResults.innerHTML = val.toString())))
+    .subscribe();
+totalPages$.subscribe(pages => (elements.totalPages.innerHTML = pages));
+userPage$.subscribe(page => {
+    elements.pageNum.innerHTML = page;
+});
+
+combineLatest(totalPages$, userPage$).subscribe(([total, userPage]) => {
+    elements.prevPage.removeAttribute('disabled');
+    elements.nextPage.removeAttribute('disabled');
+    if (userPage === 1) {
+        elements.prevPage.setAttribute('disabled', 'true');
+    }
+    if (userPage === total) {
+        elements.nextPage.setAttribute('disabled', 'true');
+    }
+});
+merge(limitLowClick$, limitMidClick$, limitHighClick$).subscribe(e => {
+    const target = e.target as HTMLButtonElement;
+    const value = target.innerHTML;
+    limit$.next(parseInt(value, 10));
+});
+limit$.subscribe(limit => {
+    [elements.limits.low, elements.limits.mid, elements.limits.high].forEach(
+        button => {
+            if (button.innerHTML === `${limit}`) {
+                button.setAttribute('disabled', 'true');
+            } else {
+                button.removeAttribute('disabled');
+            }
+        },
+    );
+});
+prevPageClick$.subscribe(() => {
+    const currentPage = page$.getValue();
+    page$.next(currentPage - 1);
+});
+
+nextPageClick$.subscribe(() => {
+    const currentPage = page$.getValue();
+    page$.next(currentPage + 1);
 });
